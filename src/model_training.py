c773656cea02f0e963d52fdc1c87ed19c67453bd
zim10 <khanazim.eee@gmail.com>
2024-12-22 01:04:16 -0500

    update model parameters

--- src/model_training.py
+++ src/model_training.py
@@ -3,7 +3,11 @@ from sklearn.ensemble import RandomForestClassifier
 def create_model(n_estimators=100):
     """Create Randomforest model"""
     return RandomForestClassifier(n_estimators=n_estimators)
+def create_model(n_estimators =150):
+    """create random forest model"""
+    return RandomForestClassifier(n_estimators=n_estimators)
 
 def train_model(model, X_train, y_train):
     """Train the model"""
     return model.fit(X_train, y_train)
+
