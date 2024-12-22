03bbaa7773e151f7b55e845975e7d53eaf2327bd
zim10 <khanazim.eee@gmail.com>
2024-12-22 01:01:08 -0500

    Improved model parameters

--- src/model_training.py
+++ src/model_training.py
@@ -7,3 +7,7 @@ def create_model(n_estimators=100):
 def train_model(model, X_train, y_train):
     """Train the model"""
     return model.fit(X_train, y_train)
+
+def create_model(n_estimators=200): # Changed default value
+    """Create improved RandomForest model"""
+    return RandomForestClassifier(n_estimators= n_estimators,max_depth=10)
