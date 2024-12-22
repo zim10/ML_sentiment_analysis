adaba2a596ace5c88a25b48a35d309bd1abad8cc
zim10 <khanazim.eee@gmail.com>
2024-12-21 23:02:37 -0500

    Add basic model structure

---
+++ src/model.py
@@ -0,0 +1,9 @@
+from sklearn.model_selection import train_test_split
+
+
+def create_model():
+    """Initialize the model"""
+    return None
+def train_model(X, y):
+    """Train the model"""
+    pass
